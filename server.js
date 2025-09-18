177a968687d9e8857450d932012e72fa211e6226
Swaminathan PL <srinath0209@gmail.com>
2025-09-18 12:57:27 +0530

    added health check

--- server.js
+++ server.js
@@ -153,6 +153,10 @@ app.use((req, res) => {
   res.status(404).send("Page not found");
 });
 
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
